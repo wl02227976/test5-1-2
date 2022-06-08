a6bf2ae64f24e9fa35cd46af1c674baae4ecf5e6
wl02227976 <b04201043@ntu.edu.tw>
2022-06-08 15:13:52 +0800

    Add files via upload

---
+++ run_experiments.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Script to reproduce results
+
+for ((i=1;i<6;i+=1))
+do 
+	python main.py \
+	--policy "TD3" \
+	--env "HalfCheetah-v3" \
+	--seed $i \
+	--save_model
+
+	python main.py \
+	--policy "TD3" \
+	--env "Hopper-v3" \
+	--seed $i \
+	--save_model
+
+	python main.py \
+	--policy "TD3" \
+	--env "Walker2d-v3" \
+	--seed $i \
+	--save_model
+
+	python main.py \
+	--policy "TD3" \
+	--env "Ant-v3" \
+	--seed $i \
+	--save_model
+done
